084a08549d5e819b284beddb8f0be1f5a1b48560
Bobby Galli <bobbyg603@gmail.com>
2017-08-17 10:44:59 -0400

    Added comments per Dave's suggestion

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 // my-node-crasher
 // https://www.npmjs.com/package/bugsplat
 // support@bugsplat.com
+
+// Required - initialize bugsplat with database name, app name, and version
 const bugsplat = require("bugsplat")("fred", "my-node-crasher", "1.0.0.0");
+
+// The following optional api methods allow further customization
 bugsplat.setAppKey("AppKey");
 bugsplat.setUser("Fred");
 bugsplat.setEmail("fred@bedrock.com");
 bugsplat.setDescription("description");
 bugsplat.addAdditionalFile("./additionalFile.txt");
 bugsplat.setCallback((error, responseBody) => {
-    console.log("BugSplat rocks!");
+    console.log("BugSplat callback!");
 });
 
+// Send an Error to BugSplat manually
+// bugsplat.post(new Error("foobar!"));
+
+// Post to BugSplat when unhandledRejections and uncaughtExceptions occur
 process.on("unhandledRejection", bugsplat.post);
 process.on("uncaughtException", bugsplat.post);
 
-// bugsplat.post(new Error("foobar!"));
-// uncaughtException();
-unhandledRejection();
+// Trigger an uncaughtException or unhandledRejection to test BugSplat
+uncaughtException();
+//unhandledRejection();
 
 function uncaughtException() {
     throw new Error("foobar!");
